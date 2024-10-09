b893732e7b9b93457c8b6c94ef6cb54892f3fb4c
ririvi <ririvi@taltech.ee>
2024-10-09 18:50:11 +0000

    CD pipeline for build & deployment

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import { ref } from 'vue'
 
 defineProps<{ msg: string }>()
@@ -15,6 +15,7 @@ const count = ref(0)
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
+    <p>test string for ci</p>
   </div>
 
   <p>
