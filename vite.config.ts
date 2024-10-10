5ee84ea7300b9208191bf26da27dfbac1a60713b
Rasmus <rraasu@taltech.ee>
2024-10-10 23:28:43 +0300

    fix: removed duplicate code from vite.config.ts.

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  },
   plugins: [vue()],
   server: {
     proxy: {
