6a46b345aafcfb1b155138e629ce32e73e821764
rkilks <148218930+rkilks@users.noreply.github.com>
2024-10-10 13:38:18 +0300

    Made some changes in structure and config.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,70 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import {inject, onMounted, ref} from "vue";
+
+const routes = ref([]);
+const axios = inject('axios')
+const route = ref({
+  id: -1,
+  name: '',
+  description: '',
+  location: '',
+  thumbnailUrl: '',
+});
+const submitForm = async () => {
+  try {
+    const response = await axios.post('/api/camping_routes', route.value);
+    console.log('Added camping route:', response.data);
+    // Reset the form after successful submission
+    route.value = {
+      name: '',
+      description: '',
+      location: '',
+      thumbnailUrl: ''
+    };
+  } catch (error) {
+    console.error('Error adding camping route:', error);
+  }
+};
+
+const fetchRoutes = async () => {
+  try {
+    const response = await axios.get('/api/camping_routes');
+    routes.value = response.data.map(route =>  ({
+      id: route.id,
+      name: route.name,
+      description: route.description,
+      location: route.location,
+      thumbnailUrl: route.thumbnailUrl,
+    }))
+  } catch (err){
+    console.error("Error fetching camping routes: " + err.message);
+  }
+}
+onMounted(fetchRoutes())
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="add-camping-route">
+    <h1>Send camping route</h1>
+    <form @submit.prevent="submitForm">
+      <div>
+        <input type="text" placeholder="Name" v-model="route.name" required>
+        <input type="text" placeholder="Description" v-model="route.description" required>
+        <input type="text" placeholder="Location" v-model="route.location" required>
+        <input type="text" placeholder="Thumbnail url" v-model="route.thumbnailUrl" required>
+      </div>
+      <button type="submit">Send</button>
+    </form>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <table v-if="routes.length">
+    <tbody>
+    <tr v-for="route in routes" :key="route.id">
+      <td> {{ route.name }} </td>
+      <td> {{ route.description }} </td>
+      <td> {{ route.location }} </td>
+    </tr>
+    </tbody>
+  </table>
 </template>
 
 <style scoped>
