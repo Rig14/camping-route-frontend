89c245ffa86e851fc0b89d63ab3865e7b3eaceae
Rasmus <rraasu@taltech.ee>
2024-10-12 15:22:54 +0300

    feat: Made searchbar usable for smaller screens as well.

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -26,12 +26,14 @@ const search = () => {
       <span class="w-auto flex justify-end  items-center text-gray-500 p-2">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-      </svg>
-    </span>
-    <input type="text" placeholder="Otsi matkaradu..." v-model="searchedValue" required class="text-white bg-transparent focus:outline-none">
-      <button type="submit" class="bg-black hover:bg-gray-700 rounded-xl text-white font-semibold text-xs text-xl p-2 pl-4 pr-4 ml-2">
-        Search
-      </button>
+        </svg>
+      </span>
+      <div class="flex justify-between w-full">
+        <input type="text" placeholder="Otsi matkaradu..." v-model="searchedValue" required class="text-white w-full bg-transparent focus:outline-none">
+        <button type="submit" class="bg-black hover:bg-gray-700 rounded-xl text-white font-semibold text-xs p-2 pl-4 pr-4 ml-2">
+          Search
+        </button>
+      </div>
     </div>
   </form>
 </template>
