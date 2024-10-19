9b5980087d34be4bd9b91c0fe93470d6801f72ba
rkilks <148218930+rkilks@users.noreply.github.com>
2024-10-19 20:45:36 +0300

    Initial gpx file input logic

--- src/views/AddRoute.vue
+++ src/views/AddRoute.vue
@@ -13,10 +13,12 @@ const route = ref<CampingRouteDto>({
   description: '',
   location: '',
   thumbnailUrl: '',
+  gpx: ''
 });
 
 const submitForm = async () => {
   try {
+    console.log(route.value)
     const response = await axios.post('/api/camping_routes', route.value);
     console.log('Added camping route:', response.data);
     // Reset the form after successful submission
@@ -24,10 +26,25 @@ const submitForm = async () => {
       name: '',
       description: '',
       location: '',
-      thumbnailUrl: ''
+      thumbnailUrl: '',
+      gpx: '',
     };
   } catch (error) {
     console.error('Error adding camping route:', error);
+  }
+};
+const handleFileChange = (event: Event) => {
+  const fileInput = event.target as HTMLInputElement;
+  const file = fileInput.files?.[0];
+
+  if (file) {
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const gpxString = e.target.result as string;
+      route.value.gpx = gpxString; // Store parsed GPX data
+    };
+    reader.readAsText(file);
   }
 };
 </script>
@@ -41,6 +58,7 @@ const submitForm = async () => {
         <input type="text" placeholder="Description" v-model="route.description" required>
         <input type="text" placeholder="Location" v-model="route.location" required>
         <input type="text" placeholder="Thumbnail url" v-model="route.thumbnailUrl" required>
+        <input type="file" @change="handleFileChange" accept=".gpx">
       </div>
       <button type="submit">Send</button>
     </form>
