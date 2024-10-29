5b5332e054209241fe12b09eec832be66f720b8b
Rihard Rivis <rihard.rivis@proton.me>
2024-10-29 18:04:49 +0200

    feat: drag and drop

--- src/components/AddCampingRouteOverlay.vue
+++ src/components/AddCampingRouteOverlay.vue
@@ -89,13 +89,28 @@ function handleAddImage(e: Event) {
           <input v-model="route.description" placeholder="Kirjeldus" required type="text">
           <input v-model="route.location" placeholder="Asukoht" required type="text">
         </div>
-        <div>
-          <input
-              accept="image/*"
-              type="file"
-              @change="handleAddImage"
-          />
+
+        <div class="flex items-center justify-center w-full">
+          <label class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600" for="dropzone-file">
+            <div class="flex flex-col items-center justify-center pt-5 pb-6">
+              <svg aria-hidden="true" class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" fill="none" viewBox="0 0 20 16" xmlns="http://www.w3.org/2000/svg">
+                <path d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+              </svg>
+              <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Vajuta</span> või lohista</p>
+              <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG, JPEG</p>
+            </div>
+            <input
+                id="dropzone-file"
+                accept="image/*"
+                class="absolute opacity-0"
+                style="width: inherit; height: inherit"
+                type="file"
+                @change="handleAddImage"
+            />
+          </label>
         </div>
+
+
         <button type="submit">Send</button>
       </form>
 
