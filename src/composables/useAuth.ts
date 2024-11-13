e035f05c3d26fbd3d2e57443185c85a016b78111
Rasmus <rraasu@taltech.ee>
2024-11-14 01:08:22 +0200

    feat: Added showAuthOverlay to authStore and removed unnecessary requireAuth function.

--- src/composables/useAuth.ts
+++ src/composables/useAuth.ts
@@ -1,23 +1,18 @@
 import { useAuthStore } from '../stores/authStore.ts'
-import { useRouter } from 'vue-router'
-import {computed} from "vue";
+import { computed } from "vue";
 
 export function useAuth() {
   const authStore = useAuthStore()
-  const router = useRouter()
-
-  const requireAuth = async () => {
-      const isAuthenticated = await authStore.checkAuth()
-      if (!isAuthenticated) {
-        await router.push('/')
-      }
-  }
 
   const isLoggedIn = computed(() => authStore.isLoggedIn);
+  const showAuthOverlay = computed({
+    get: () => authStore.showAuthOverlay,
+    set: (value: boolean) => { authStore.showAuthOverlay = value; }
+  });
 
   return {
     isLoggedIn,
-    requireAuth,
+    showAuthOverlay,
     login: authStore.login,
     signup: authStore.signup,
     logout: authStore.logout
