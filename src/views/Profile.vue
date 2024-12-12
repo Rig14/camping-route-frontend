14c0e279150a67cbaa9885889fce8e5b3a901cea
Rihard Rivis <rihard.rivis@proton.me>
2024-12-12 12:48:10 +0200

    feat: Use case 3
    User can see their comments under their profile page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -8,6 +8,8 @@ import {PageResponse} from "../types/dto/PageRespone.ts";
 import {getImageUrlsForId} from "../util/images.ts";
 import {useRoute, useRouter} from "vue-router";
 import {useAuth} from "../composables/useAuth.ts";
+import {CommentDto} from "../types/dto/CommentDto.ts";
+import CommentCard from "../components/CommentCard.vue";
 
 const { logout, getUserId } = useAuth();
 
@@ -28,6 +30,10 @@ const currentPage = ref<number>(1);
 const pageSize = 6;
 const totalPages = ref<number>(1);
 const totalElements = ref<number>(1);
+
+const viewSelection = ref<string>('comments');
+
+const comments = ref<CommentDto[]>([]);
 
 const fetchUser = async () => {
   try {
@@ -112,8 +118,22 @@ const nextPage = () => {
   }
 };
 
+const fetchUserComments = async () => {
+  try {
+    const response = await axios.get<CommentDto[]>(`/api/public/camping_routes/comments/user/${route.params.id}`);
+    comments.value = response.data;
+  } catch (error) {
+    console.error("Error fetching comments: " + error);
+  }
+}
+
+const switchView = (viewName: string) => {
+  viewSelection.value = viewName;
+}
+
 onMounted(() => {
   fetchUser();
+  fetchUserComments();
   fetchUserCampingRoutes(currentPage.value)
 })
 
@@ -135,9 +155,9 @@ watch(
         <div class="flex items-center justify-center gap-4">
           <div class="flex justify-center">
             <img
+                alt="User Avatar"
                 class="w-24 h-24 rounded-full border-2 border-gray-300"
                 src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.murrayglass.com%2Fwp-content%2Fuploads%2F2020%2F10%2Favatar-1024x1024.jpeg&f=1&nofb=1&ipt=2895779559f3c219d7b09907eff55a6b3f42d271f2548b8ff49e9021dd738d2e&ipo=images"
-                alt="User Avatar"
             />
           </div>
           <div class="flex flex-col gap-2 items-center sm:items-start text-center sm:text-left">
@@ -158,38 +178,49 @@ watch(
     <div v-if="isLoading">Loading...</div>
     <div v-else-if="userCampingRoutes.length == 0">No results found.</div>
     <div v-else>
-      <div
-          :class="[
+      <div class="flex gap-4 justify-center mb-4">
+        <button :class="{'bg-emerald-600': viewSelection == 'comments'}" @click="switchView('comments')">Kommentaarid</button>
+        <button :class="{'bg-emerald-600': viewSelection == 'routes'}" @click="switchView('routes')">Matkarajad</button>
+      </div>
+      <div v-if="viewSelection == 'routes'">
+        <div
+            :class="[
       'grid gap-6',
       userCampingRoutes.length === 1
         ? 'grid-cols-1 place-items-center'
         : 'grid-cols-1 md:grid-cols-2 lg:grid-cols-3'
     ]"
-      >
-        <div v-for="item in userCampingRoutes" :key="item.route.id">
-          <RouterLink :to="{ name: 'CampingRoute', params: { id: item.route.id } }">
-            <CampingRouteCard :camping-route="item.route" :image-urls="item.images" />
-          </RouterLink>
+        >
+          <div v-for="item in userCampingRoutes" :key="item.route.id">
+            <RouterLink :to="{ name: 'CampingRoute', params: { id: item.route.id } }">
+              <CampingRouteCard :camping-route="item.route" :image-urls="item.images" />
+            </RouterLink>
+          </div>
+        </div>
+        <div class="px-6 py-4 flex justify-center items-center">
+          <button
+              :disabled="currentPage === 1"
+              class="px-4 py-2 rounded disabled:opacity-50 mr-2"
+              @click="prevPage"
+          >
+            Previous
+          </button>
+          <span class="text-gray-300 text-base">
+            Page {{ currentPage }} of {{ totalPages }}
+          </span>
+          <button
+              :disabled="currentPage === totalPages"
+              class="px-4 py-2 rounded disabled:opacity-50 ml-2"
+              @click="nextPage"
+          >
+            Next
+          </button>
         </div>
       </div>
-      <div class="px-6 py-4 flex justify-center items-center">
-        <button
-            :disabled="currentPage === 1"
-            class="px-4 py-2 rounded disabled:opacity-50 mr-2"
-            @click="prevPage"
-        >
-          Previous
-        </button>
-        <span class="text-gray-300 text-base">
-        Page {{ currentPage }} of {{ totalPages }}
-      </span>
-        <button
-            :disabled="currentPage === totalPages"
-            class="px-4 py-2 rounded disabled:opacity-50 ml-2"
-            @click="nextPage"
-        >
-          Next
-        </button>
+      <div v-if="viewSelection == 'comments'">
+        <div v-for="comment in comments" :key="comment.id">
+          <CommentCard :comment="comment" />
+        </div>
       </div>
     </div>
   </div>
